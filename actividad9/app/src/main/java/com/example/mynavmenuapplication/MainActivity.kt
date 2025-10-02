f5549d1451ccc7bdb7506ca37c7fd439cd1a684c
Gabriela1029 <gabriela.robles01@unicatolica.edu.co>
2025-10-01 20:33:55 -0500

    subir la actividad de la semana 9

---
+++ actividad9/app/src/main/java/com/example/mynavmenuapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mynavmenuapplication
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.mynavmenuapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Fragment inicial
+        replaceFragment(HomeFragment())
+
+        // Listener de navegación
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_about -> replaceFragment(AboutFragment())
+                R.id.nav_services -> replaceFragment(ServicesFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
